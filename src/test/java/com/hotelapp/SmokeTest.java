018293b354781cef9f19773a0047d65dabbb9606
siewong007 <siewong007@gmail.com>
2026-08-22 02:51:49 +0800

    build: maven scaffold and application entrypoint

---
+++ src/test/java/com/hotelapp/SmokeTest.java
@@ -0,0 +1,14 @@
+package com.hotelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
+class SmokeTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
